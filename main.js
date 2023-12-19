c9c64f320a7fdf613e1ec8ad84065f1237bc8f3c
Feera789 <feera.yunusova-97@mail.ru>
2023-12-19 21:57:22 +0500

    new file

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hello JS');
+function sumOfAll(array1, array2) {
+   let sum1 = 0
+   let sum2 = 0
+   for (let index = 0; index < array1.length; index++) {
+       sum1 += array1[index]
+   }
+   for (let index = 0; index < array2.length; index++) {
+    sum2 += array2[index]
+}
+return sum1 + sum2
+}
+
+console.log(sumOfAll([1,2], [3,4]));
+
+
